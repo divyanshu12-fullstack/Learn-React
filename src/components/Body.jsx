730869d80852a5fd7ecf096a0ff8053889f1cc3b
Divyanshu Dwivedi <divyanshudwivedi1290@gmail.com>
2025-07-11 00:33:06 +0530

    Data is now fetched dynamically from swiggy backened API

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,24 @@
-import RestaurantsList from "../utils/RestaurantList";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(RestaurantsList);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.836063&lng=81.007955&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setRestaurantList(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="body">
       <div className="filter">
